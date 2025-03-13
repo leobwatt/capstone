5574e6d347d60284c45d9b982295d9f8b045eb1b
KianKhadempour <kiankhadempour@gmail.com>
2025-03-13 11:32:11 -0700

    Add range detector script and calibrate color range so it works better

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import numpy as np
 
 BUFFER_SIZE = 64
 
-red_lower = np.array((224, 125, 146))
-red_upper = np.array((255, 141, 159))
+
+red_lower = np.array((147, 141, 146))
+red_upper = np.array((182, 175, 255))
 points: deque[tuple[int, int]] = deque(maxlen=BUFFER_SIZE)
 
 capture = cv2.VideoCapture(0)
+prev_center = None
 
 while True:
     _, frame = capture.read()
@@ -20,22 +22,21 @@ while True:
     mask = cv2.inRange(hsv, red_lower, red_upper)
     mask = cv2.erode(mask, np.zeros(1), iterations=2)
     mask = cv2.dilate(mask, np.zeros(1), iterations=2)
+    cv2.imwrite("test.png", mask)
 
     contours = cv2.findContours(
         mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )[0]
     center = (0, 0)
-
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         m = cv2.moments(c)
-        center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
+        center = (int(m["m10"] / (m["m00"] + 0.01)), int(m["m01"] / (m["m00"] + 0.01)))
 
         if radius > 10:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
     points.appendleft(center)
 
     for i in range(1, len(points)):
